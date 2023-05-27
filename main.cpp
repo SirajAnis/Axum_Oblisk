348ec7a305838300ce8ddc97f052ec7cee67f65e
Siraj Anis <www.sirajanis99@mail.com>
2023-05-27 04:52:04 -0900

    fourth

--- main.cpp
+++ main.cpp
@@ -5,15 +5,15 @@
 void init() {
 	glClearColor(0.4, 0.8, 0.9, 1);
 	glMatrixMode(GL_PROJECTION);
-	glOrtho(-40, 40, -44, 40, -40, 40);
+	glOrtho(-40, 40, -40, 41, -40, 40);
 }
 
 void display() {
 	glClear(GL_COLOR_BUFFER_BIT);
 	glColor3f(0, 0.6, 0);
-	glRectf(-40, -18, 40, -43);
+	glRectf(-40, -16, 40, -43);
 
-	glColor3f(0.480, 0.304, 0.298);
+	glColor3f(0.460, 0.630, 0.468);
 	glRectf(-40, -23, 40, -44);
 
 	glColor3f(0.7, 0.6, 0.6);
@@ -31,24 +31,72 @@ void display() {
 	glVertex2f(-2.5, -22);
 	glEnd();
 
-	glPushMatrix();
 	glColor3f(0.840, 0.722, 0.722);
-	glTranslatef(-4.58, 37.5, 0);
-	glutSolidCone(2, 2, 20, 20);
+	glBegin(GL_POLYGON);
+	for (int i = 0; i < 360; i++)
+	{
+		float angle = i * 3.14 / 180;
+		float x = 2 * cos(angle) - 4.6;
+		float y = 4.8 * sin(angle) + 36;
+		glVertex2f(x, y);
+	}
+	glEnd();
+	glColor3f(0.640, 0.518, 0.543);
+	glBegin(GL_POLYGON);
+	for (int i = 0; i < 360; i++)
+	{
+		float angle = i * 3.14 / 180;
+		float x = 1.7 * cos(angle) - 4.6;
+		float y = 4.5 * sin(angle) + 35.5;
+		glVertex2f(x, y);
+	}
+	glEnd();
 
-	//glColor3f(0.4, 0.8, 0.9);
-	//glTranslatef(2.35, -1.5, 0);
-	//glutSolidCone(1, 2, 20, 20);
-	//glTranslatef(-4.4, 0, 0);
-	//glutSolidCone(1, 2, 20, 20);
-	glPopMatrix();
+	glColor3f(0.840, 0.722, 0.722);
+	glBegin(GL_POLYGON);
+	for (int i = 0; i < 360; i++)
+	{
+		float angle = i * 3.14 / 180;
+		float x = 1.6 * cos(angle) - 4.6;
+		float y = 5 * sin(angle) + 34.5;
+		glVertex2f(x, y);
+	}
+	glEnd();
+
 	glPushMatrix();
 	glColor3f(0.4, 0.8, 0.9);
-	glTranslatef(-6.48, 36.3, 0);
+	glTranslatef(-6.6, 36, 0);
 	GLUquadricObj* objj2 = gluNewQuadric();
-	gluPartialDisk(objj2, 0, 0.7, 50, 90, 105, -180);
+	gluPartialDisk(objj2, 0, 0.9, 50, 90, 110, -180);
 	glPopMatrix();
 
+	glPushMatrix();
+	glTranslatef(-2.35, 36.2, 0);
+	GLUquadricObj* objj3 = gluNewQuadric();
+	gluPartialDisk(objj3, 0, 0.9, 50, 90, 70, -180);
+	glPopMatrix();
+
+	glLineWidth(3.5);
+	glColor3f(0.640, 0.518, 0.543);
+	glBegin(GL_LINE_STRIP); //on the top
+	glVertex2f(-3.6, 36);//right
+	glVertex2f(-3.55, 37);
+	glVertex2f(-3.1, 37.5);
+	glEnd();
+	glBegin(GL_LINE_STRIP); //on the top
+	glVertex2f(-5.4, 36);//left
+	glVertex2f(-5.45, 36.9);
+	glVertex2f(-6.1, 37.6);
+	glEnd();
+
+	glLineWidth(4);//on top
+	glColor3f(0.840, 0.722, 0.722);
+	glBegin(GL_LINES);
+	glVertex2f(-5.8, 36.9);
+	glVertex2f(-6.5, 37.1);
+	glVertex2f(-3.4, 36.6);
+	glVertex2f(-2.9, 37.3);
+	glEnd();
 
 	glBegin(GL_QUADS);
 	glColor3f(0.680, 0.666, 0.669);//RS from levels
@@ -56,7 +104,6 @@ void display() {
 	glVertex2f(7, -44);
 	glVertex2f(12, -44);
 	glVertex2f(12, -29);
-
 	glEnd();
 
 	glLineWidth(3);
@@ -83,7 +130,31 @@ void display() {
 	glVertex2f(10.8, -34.5);
 	glVertex2f(10.8, -37.5);
 	glEnd();
-	glBegin(GL_QUADS);
+
+	glBegin(GL_QUADS);//very small
+	glColor3f(0.840, 0.722, 0.722);//right
+	glVertex2f(3, -24);//1st
+	glVertex2f(3, -18);
+	glVertex2f(1, -18);
+	glVertex2f(1, -24);
+	glColor3f(0.7, 0.7, 0.7);//top
+	glVertex2f(-1, -21);
+	glVertex2f(3, -21);
+	glVertex2f(9, -25);
+	glVertex2f(7, -25);
+	glColor3f(0.840, 0.722, 0.722);
+	glVertex2f(6, -23);//2nd
+	glVertex2f(6, -20);
+	glVertex2f(4, -20);
+	glVertex2f(4, -23);
+	glVertex2f(9, -28);//3rd
+	glVertex2f(9, -22);
+	glVertex2f(7, -22);
+	glVertex2f(7, -28);
+	glVertex2f(1, -21);//side
+	glVertex2f(1, -24);
+	glVertex2f(7, -28);
+	glVertex2f(7, -25);
 	//level
 	glColor3f(0.94, 0.86, 0.81);
 	glVertex2f(-10, -21);
@@ -168,14 +239,13 @@ void display() {
 	glVertex2f(-8, -44);
 	glVertex2f(12, -44);
 	glVertex2f(12, -41);
-
+	glColor3f(0.840, 0.722, 0.722);//obliscks
 	//back small 1
-	glColor3f(0.720, 0.684, 0.684);
 	glVertex2f(-36.7, -24);
 	glVertex2f(-37, -12);
 	glVertex2f(-39, -12);
 	glVertex2f(-39.3, -24);
-
+	//top
 	glVertex2f(-37, -12);
 	glVertex2f(-37.7, -11);
 	glVertex2f(-38.3, -11);
@@ -185,7 +255,7 @@ void display() {
 	glVertex2f(-22, -9);
 	glVertex2f(-24, -9);
 	glVertex2f(-24.3, -24);
-
+	//top
 	glVertex2f(-22, -9);
 	glVertex2f(-22.7, -8);
 	glVertex2f(-23.3, -8);
@@ -195,7 +265,7 @@ void display() {
 	glVertex2f(10, -7);
 	glVertex2f(12, -7);
 	glVertex2f(12.3, -24);
-
+	//top
 	glVertex2f(10, -7);
 	glVertex2f(10.7, -6);
 	glVertex2f(11.3, -6);
@@ -205,63 +275,85 @@ void display() {
 	glVertex2f(26, -11);
 	glVertex2f(28, -11);
 	glVertex2f(28.3, -24);
-
+	//top
 	glVertex2f(26, -11);
 	glVertex2f(26.7, -10);
 	glVertex2f(27.3, -10);
 	glVertex2f(28, -11);
-	//first
+	//first big
 	glVertex2f(-29.7, -27.5);
 	glVertex2f(-30, -4);
 	glVertex2f(-33, -4);
 	glVertex2f(-33.3, -27.5);
-
-	/*glVertex2f(-30, -4);
-	glVertex2f(-31, -2);
-	glVertex2f(-32, -2);
-	glVertex2f(-33, -4);*/
-	//second
-	glVertex2f(-14.5, -27.5);
-	glVertex2f(-15, 0);
-	glVertex2f(-18, 0);
-	glVertex2f(-18.5, -27.5);
-
-	/*glVertex2f(-15, 0);
-	glVertex2f(-16, 2);
-	glVertex2f(-17, 2);
-	glVertex2f(-18, 0);*/
-
 	//third bigest
 	glColor3f(0.840, 0.722, 0.722);
 	glVertex2f(-8, -22);
 	glVertex2f(-6.5, 36);
 	glVertex2f(-2.5, 36);
 	glVertex2f(-1, -22);
-	//fourth
+	//fourth big
 	glVertex2f(15.4, -27.5);
 	glVertex2f(16, 3);
 	glVertex2f(19, 3);
 	glVertex2f(19.6, -27.5);
-	/*
-	glVertex2f(10,6);
-	glVertex2f(11,8);
-	glVertex2f(12,8);
-	glVertex2f(13,6);*/
-	//fifth
+	//fifth big
 	glVertex2f(32.5, -27.5);
 	glVertex2f(33, -2);
 	glVertex2f(36, -2);
 	glVertex2f(36.5, -27.5);
-
-	/*glVertex2f(33, -2);
-	glVertex2f(34, 0);
-	glVertex2f(35, 0);
-	glVertex2f(36, -2);*/
-
 	glEnd();
+
 	glColor3f(0.850, 0.842, 0.842);
 	glRectf(-40, -29, -8, -44);
 	glRectf(12, -29, 40, -44);
+
+	glBegin(GL_QUADS);//very small
+	glColor3f(0.840, 0.722, 0.722);//left
+	glVertex2f(-14.5, -24);//1st
+	glVertex2f(-14.5, -18);
+	glVertex2f(-17, -18);
+	glVertex2f(-17, -24);
+	glColor3f(0.7, 0.7, 0.7);//top
+	glVertex2f(-17, -21);
+	glVertex2f(-15, -21);
+	glVertex2f(-9, -25);
+	glVertex2f(-11, -25);
+	glColor3f(0.840, 0.722, 0.722);
+	glVertex2f(-12, -23);//2nd
+	glVertex2f(-12, -20);
+	glVertex2f(-14, -20);
+	glVertex2f(-14, -23);
+	glVertex2f(-9, -28);//3rd
+	glVertex2f(-9, -22);
+	glVertex2f(-11, -22);
+	glVertex2f(-11, -28);
+	glVertex2f(-17, -21);//side
+	glVertex2f(-17, -24);
+	glVertex2f(-11, -28);
+	glVertex2f(-11, -25);
+	//second big
+	glColor3f(0.840, 0.722, 0.722);
+	glVertex2f(-14.5, -27.5);
+	glVertex2f(-15, 0);
+	glVertex2f(-18, 0);
+	glVertex2f(-18.5, -27.5);
+	glEnd();
+
+	glPushMatrix();//top of the shortest oblisck
+	glColor3f(0.840, 0.722, 0.722);
+	glTranslatef(2, -17.7, 0);
+	glutSolidCone(1, 2, 20, 20);
+	glTranslatef(3, -2.1, 0);
+	glutSolidCone(1, 2, 20, 20);
+	glTranslatef(3, -2.1, 0);
+	glutSolidCone(1, 2, 20, 20);
+	glTranslatef(-18, 0, 0);
+	glutSolidCone(1, 2, 20, 20);
+	glTranslatef(-3, 2.1, 0);
+	glutSolidCone(1, 2, 20, 20);
+	glTranslatef(-2.5, 2.1, 0);
+	glutSolidCone(1, 2, 20, 20);
+	glPopMatrix();
 
 	glLineWidth(1.5);
 	glBegin(GL_LINES);
@@ -278,7 +370,7 @@ void display() {
 	glVertex2f(-5.7, -22);
 	glVertex2f(-3.5, 36);
 	glVertex2f(-3.3, -22);
-	//
+	//Horizontal line on the biggest
 	glVertex2f(-6.7, 30);//1
 	glVertex2f(-5.5, 30);
 	glVertex2f(-5.5, 29.6);
@@ -375,18 +467,16 @@ void display() {
 	glVertex2f(-3.5, -12.9);
 	glVertex2f(-3.5, -12.5);
 	glVertex2f(-1.2, -12.5);
-
 	glVertex2f(-7.8, -18);//9
 	glVertex2f(-5.5, -18);
 	glVertex2f(-3.5, -18);
 	glVertex2f(-1.2, -18);
-
 	glEnd();
 
-	//glLineWidth(3);
+	glLineWidth(1.5);
 	glBegin(GL_LINES);
-	glVertex2f(-5.2, 35.5);//hole 1
-	glVertex2f(-3.8, 35.5);
+	glVertex2f(-5.4, 35.5);//hole 1
+	glVertex2f(-3.6, 35.5);
 	glVertex2f(-5.25, 35.1);
 	glVertex2f(-4, 35.1);
 	glVertex2f(-4, 35.1);
@@ -459,7 +549,7 @@ void display() {
 	glVertex2f(-3.8, 2);
 	glVertex2f(-5.24, -1.3);//hole 7
 	glVertex2f(-3.8, -1.3);
-    glVertex2f(-5.3, -1.7);
+	glVertex2f(-5.3, -1.7);
 	glVertex2f(-4, -1.7);
 	glVertex2f(-4, -1.8);
 	glVertex2f(-4, -4.3);
@@ -481,56 +571,57 @@ void display() {
 	glVertex2f(-3.8, -11.3);
 	glVertex2f(-5.3, -11.3);
 	glVertex2f(-3.8, -11.3);
-
 	glEnd();
-    //center on the longest oblisk
-    glColor3f(0.740, 0.629, 0.629);
-	glRectf(-5,32.5,-4.2,34.8);
-	glRectf(-5,26.5,-4.2,28.6);
-	glRectf(-5,20.7,-4.2,22.6);
-	glRectf(-5.2,14.6,-4.2,16.6);
-    glRectf(-5.2,8.6,-4.2,10.6);
-    glRectf(-5.2,2.3,-4.2,4.6);
-    glRectf(-5.2,-4.3,-4.2,-1.9);
-    glRectf(-5.2,-11,-4.2,-8.5);
-    glRectf(-5.25,-14.8,-3.7,-21.65);
-    glRectf(-4.7,-17,-4.3,-20);
-    glColor3f(0.600, 0.504, 0.504);
-    glRectf(-5,-16,-4,-21);
-    glColor3f(0.670, 0.543, 0.543);
-    glRectf(-4.7,-17,-4.3,-20);
+	//center on the longest oblisk
+	glColor3f(0.740, 0.629, 0.629);
+	glRectf(-5, 32.5, -4.2, 34.8);
+	glRectf(-5, 26.5, -4.2, 28.6);
+	glRectf(-5, 20.7, -4.2, 22.6);
+	glRectf(-5.2, 14.6, -4.2, 16.6);
+	glRectf(-5.2, 8.6, -4.2, 10.6);
+	glRectf(-5.2, 2.3, -4.2, 4.6);
+	glRectf(-5.2, -4.3, -4.2, -1.9);
+	glRectf(-5.2, -11, -4.2, -8.5);
+	glRectf(-5.25, -14.8, -3.7, -21.65);
+	glColor3f(0.600, 0.504, 0.504);
+	glRectf(-5, -16, -4, -21);
+	glColor3f(0.670, 0.543, 0.543);
+	glRectf(-4.7, -17, -4.3, -20);
+	//left side on the longest oblisk
+	glColor3f(0.74, 0.62, 0.62);
+	glRectf(-6.5, 32.3, -5.65, 35.4);
+	glRectf(-6.62, 26.3, -5.7, 29.7);
+	glRectf(-6.7, 20.4, -5.7, 23.7);
+	glRectf(-6.9, 14.4, -5.75, 17.8);
+	glRectf(-7, 8.5, -5.75, 11.8);
+	glRectf(-7.2, 2.25, -5.75, 5.8);
+	glRectf(-7.35, -4.35, -5.85, -0.7);
+	glRectf(-7.55, -11.25, -5.85, -7.4);
+	glRectf(-7.7, -17.55, -5.85, -14.4);
+	glRectf(-7.75, -21.65, -5.9, -18.4);
+	glColor3f(0.620, 0.477, 0.477);
+	glRectf(-7.4, -16.5, -6.1, -15.2);
+	glRectf(-7.4, -20.7, -6.1, -19.4);
+	//right side on the longest oblisk
+	glColor3f(0.74, 0.62, 0.62);
+	glRectf(-3.3, 32.3, -2.6, 35.4);
+	glRectf(-3.3, 26.3, -2.45, 29.7);
+	glRectf(-3.25, 20.4, -2.25, 23.7);
+	glRectf(-3.25, 14.4, -2.15, 17.8);
+	glRectf(-3.25, 8.4, -2, 11.8);
+	glRectf(-3.25, 2.25, -1.75, 5.8);
+	glRectf(-3.25, -4.35, -1.65, -0.75);
+	glRectf(-3.18, -11.25, -1.55, -7.4);
+	glRectf(-3.15, -17.55, -1.4, -14.4);
+	glRectf(-3.15, -21.65, -1.4, -18.3);
+	glColor3f(0.620, 0.477, 0.477);
+	glRectf(-2.95, -16.5, -1.65, -15.2);
+	glRectf(-2.95, -20.7, -1.65, -19.4);
 
-    //left side on the longest oblisk
-    glColor3f(0.74, 0.62, 0.62);
-    glRectf(-6.5,32.3,-5.65,35.4);
-    glRectf(-6.62,26.3,-5.7,29.7);
-    glRectf(-6.7,20.4,-5.7,23.7);
-    glRectf(-6.9,14.4,-5.75,17.8);
-    glRectf(-7,8.5,-5.75,11.8);
-    glRectf(-7.2,2.25,-5.75,5.8);
-    glRectf(-7.35,-4.35,-5.85,-0.7);
-    glRectf(-7.55,-11.25,-5.85,-7.4);
-    glRectf(-7.7,-17.55,-5.85,-14.4);
-    glRectf(-7.75,-21.65,-5.9,-18.4);
-    glColor3f(0,0,0);
-    glRectf(-7.4,-16.5,-6.1,-15.2);
-
-    //right side on the longest oblisk
-    glColor3f(0.74, 0.62, 0.62);
-    glRectf(-3.3,32.3,-2.6,35.4);
-    glRectf(-3.3,26.3,-2.45,29.7);
-    glRectf(-3.25,20.4,-2.25,23.7);
-    glRectf(-3.25,14.4,-2.15,17.8);
-    glRectf(-3.25,8.4,-2,11.8);
-    glRectf(-3.25,2.25,-1.75,5.8);
-    glRectf(-3.25,-4.35,-1.65,-0.75);
-    glRectf(-3.18,-11.25,-1.55,-7.4);
-    glRectf(-3.15,-17.55,-1.4,-14.4);
-    glRectf(-3.15,-21.65,-1.4,-18.3);
-
+	//lines on the left side of the biggest
 	glLineWidth(2);
 	glBegin(GL_LINES);
-	glColor3f(0.840, 0.722, 0.722);//lines on the left side of the biggest
+	glColor3f(0.840, 0.722, 0.722);
 	glVertex2f(-5.6, 35);
 	glVertex2f(-6.55, 35);
 	glVertex2f(-5.6, 29.2);
@@ -550,7 +641,8 @@ void display() {
 	glColor3f(0.600, 0.504, 0.504);
 	glVertex2f(-5.35, -15.2);
 	glVertex2f(-3.6, -15.2);
-	glColor3f(0.840, 0.722, 0.722);//lines on the right side of the biggest
+	//lines on the right side of the biggest
+	glColor3f(0.840, 0.722, 0.722);
 	glVertex2f(-3.3, 35);
 	glVertex2f(-2.5, 35);
 	glVertex2f(-3.3, 29.2);
@@ -804,13 +896,12 @@ void display() {
 	glVertex2f(36, -44);
 	glVertex2f(39.3, -41);
 	glVertex2f(39.3, -44);
-
 	glEnd();
+
 	glPushMatrix();
 	glColor3f(0.840, 0.722, 0.722);
 	glTranslatef(17.5, 3.5, 0);
-	glutSolidCone(1.5, 2, 20, 20);
-
+	glutSolidCone(1.5, 2, 20, 20);//
 	glColor3f(0.840, 0.722, 0.722);
 	glTranslatef(17, -5.2, 0);
 	glutSolidCone(1.5, 2, 20, 20);
@@ -933,15 +1024,13 @@ void display() {
 	gluPartialDisk(objj1, 0, 1.55, 50, 90, 90, -180);
 	glPopMatrix();
 
-    //glColor3f(1,0.5,0);
-
 	glBegin(GL_POLYGON);
-	for(int i=0;i<360;i++)
-    {
-        float angle=i*3.14/180;
-        float x=1.45*cos(angle)+17.48;
-        float y=3*sin(angle)+3.9;
-        glVertex2f(x,y);
+	for (int i = 0; i < 360; i++)
+	{
+		float angle = i * 3.14 / 180;
+		float x = 1.45 * cos(angle) + 17.48;
+		float y = 3 * sin(angle) + 3.9;
+		glVertex2f(x, y);
 	}
 	glEnd();
 	glFlush();
